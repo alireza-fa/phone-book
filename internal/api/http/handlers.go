e5e7b48192d5d6d498864fe0f35388a8fbe8669f
alireza-fa <alirezafeyze44@gmail.com>
2024-02-25 21:24:37 +0330

    complete users handlers

--- internal/api/http/handlers.go
+++ internal/api/http/handlers.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"github.com/alireza-fa/phone-book/internal/models"
+	"github.com/alireza-fa/phone-book/pkg/rdbms"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 	"net/http"
@@ -14,11 +16,29 @@ func (handler *Server) register(c *fiber.Ctx) error {
 		return c.Status(http.StatusBadRequest).SendString(errString)
 	}
 
-	// TODO: if user with this email exists ot not
+	user, err := handler.repository.GetUserByEmail(request.Email)
+	if err != nil && err.Error() != rdbms.ErrNotFound {
+		errString := "Error while retrieving data from database"
+		handler.logger.Error(errString, zap.Error(err))
+		return c.Status(http.StatusInternalServerError).SendString(errString)
+	} else if err == nil || user != nil && user.Id != 0 {
+		errString := "User with given email already exists"
+		handler.logger.Error(errString, zap.String("email", request.Email))
+		return c.Status(http.StatusBadRequest).SendString(errString)
+	}
 
-	// TODO: create user
+	user = &models.User{Email: request.Email, Password: request.Password}
+	if err := handler.repository.CreateUser(user); err != nil {
+		errString := "Error happened while creating the user"
+		handler.logger.Error(errString, zap.Error(err))
+		return c.Status(http.StatusInternalServerError).SendString(errString)
+	} else if user.Id == 0 {
+		errString := "Error invalid user id created"
+		handler.logger.Error(errString, zap.Any("user", user))
+		return c.Status(http.StatusInternalServerError).SendString(errString)
+	}
 
-	token, err := handler.token.CreateTokenString(1)
+	token, err := handler.token.CreateTokenString(user.Id)
 	if err != nil {
 		errString := "Error creating JWT token for user"
 		handler.logger.Error(errString, zap.Error(err))
@@ -37,9 +57,18 @@ func (handler *Server) login(c *fiber.Ctx) error {
 		return c.Status(http.StatusBadRequest).SendString(errString)
 	}
 
-	// TODO: if user with combination of email and password exists or not
+	user, err := handler.repository.GetUserByEmailAndPassword(request.Email, request.Password)
+	if err != nil {
+		errString := "wrong email or password has been given"
+		handler.logger.Error(errString, zap.Error(err))
+		return c.Status(http.StatusBadRequest).SendString(errString)
+	} else if user == nil {
+		errString := "Error invalid user retured"
+		handler.logger.Error(errString, zap.Any("request", request))
+		return c.Status(http.StatusInternalServerError).SendString(errString)
+	}
 
-	token, err := handler.token.CreateTokenString(1)
+	token, err := handler.token.CreateTokenString(user.Id)
 	if err != nil {
 		errString := "Error creating JWT token for user"
 		handler.logger.Error(errString, zap.Error(err))
